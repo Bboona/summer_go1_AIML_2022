bc375bd233abc4756f5a3a59506b3bbec69cddec
Bboona <borna.m500@yahoo.com>
2023-01-04 10:20:00 +1030

    Added README.md and .gitignore, removed /build & /devel

--- catkin_ws/build/greenball_tracking/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/unitree/summer_project/catkin_ws/src/greenball_tracking
-# Build directory: /home/unitree/summer_project/catkin_ws/build/greenball_tracking
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
