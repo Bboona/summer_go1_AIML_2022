2c3322830ff1f7660f4b0a97a716f37f23d7d451
Bboona <borna.m500@yahoo.com>
2023-01-06 16:15:37 +1030

    Optimizing the proccess creation and killing

--- catkin_ws/src/greenball_tracking/src/joystick_node.cc
+++ catkin_ws/src/greenball_tracking/src/joystick_node.cc
@@ -17,6 +17,7 @@ Use of this source code is governed by the MPL-2.0 license, see LICENSE.
 using namespace UNITREE_LEGGED_SDK;
 
 bool running = false;
+int pid = 0;  // Initialize the child process ID to 0
 
 class Joystick
 {
@@ -60,11 +61,24 @@ void Joystick::RobotControl()
     memcpy(&_keyData, &state.wirelessRemote[0], 40);
 
     if((int)_keyData.btn.components.L2 == 1 && (int)_keyData.btn.components.R2 == 1){
-        std::cout << "Starting green_ball_tracking Node..."<< std::endl;
         // Run the greenball_tracking ROS node using the system() function
         if (!running){
+            std::cout << "Starting green_ball_tracking Node..."<< std::endl;
             running = true;
-            pid = system("rosrun greenball_tracking greenball_tracking 1 &");
+            pid = fork();
+
+            if (pid == 0) {
+                // This is the child process
+                execl("/usr/bin/nohup", "/usr/bin/nohup", "rosrun", "greenball_tracking", "greenball_tracking", "1", (char*)NULL);
+                exit(EXIT_SUCCESS);
+            } else if (pid > 0) {
+                // This is the parent process
+                std::cout << "Child process created with PID " << pid << std::endl;
+            } else {
+                // fork() failed
+                std::cerr << "fork() failed" << std::endl;
+                exit(EXIT_FAILURE);
+            }
             mqtt.setColor(0, 0, 255);
         }
         
@@ -72,8 +86,6 @@ void Joystick::RobotControl()
     if((int)_keyData.btn.components.L2 == 1 && (int)_keyData.btn.components.L1 == 1 && running){
         std::cout << "Stopping green_ball_tracking Node..."<< std::endl;
         mqtt.setColor(0, 0, 0);
-        // Run the greenball_tracking ROS node using the system() function
-        cout<<pid<<endl;
         kill(pid, SIGTERM);
         running = false;
     }
@@ -83,7 +95,6 @@ void Joystick::RobotControl()
 
 int main(void)
 {
-
     Joystick custom(HIGHLEVEL);
     // InitEnvironment();
     LoopFunc loop_control("control_loop", custom.dt,    boost::bind(&Joystick::RobotControl, &custom));
